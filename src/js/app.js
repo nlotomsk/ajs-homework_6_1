dda60f15c60bae35a1193788a3e6687fe26fa905
Vyacheslav <93542374+nlotomsk@users.noreply.github.com>
2023-07-26 14:46:07 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ export default class Character {
     }
     this.health = 100;
     this.level = 1;
-    this.attack = 25;
-    this.defence = 25;
+    this.attack = undefined;
+    this.defence = undefined;
   }
 }
